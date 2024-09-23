3d91c1b0d167b6446692a3697b1d604c24a7fcea
OppSpark <picks5@naver.com>
2024-09-23 13:50:21 +0900

    3week1

--- 2024_mobile/app/src/main/java/com/example/sample1week/MainActivity.java
+++ 2024_mobile/app/src/main/java/com/example/sample1week/MainActivity.java
@@ -1,31 +1,40 @@
 package com.example.sample1week;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.view.View;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.GravityCompat;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
     private String str = "Mobile Programming";
     private float textSize = 33;
+    private Random random = new Random();
+
+    private int[] onChangeColor() {
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+        return new int[]{r, g, b};
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.tv);
 
-        TextView tv = new TextView(this);
+        textView.setText(str);
+        textView.setTextSize(textSize);
 
-        tv.setText(str);
-        tv.setTextSize(textSize);
-        tv.setGravity(Gravity.CENTER);
-
-        setContentView(tv);
+        textView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int[] color = onChangeColor();
+                textView.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
+            }
+        });
     }
 }
